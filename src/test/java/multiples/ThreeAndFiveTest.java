5ade177551685828bd34bd26f25992573ff1652b
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-11-05 15:10:30 +0000

    Initial commit

---
+++ src/test/java/multiples/ThreeAndFiveTest.java
@@ -0,0 +1,18 @@
+package multiples;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Created by Sylvester Abreu on 05/11/2015.
+ */
+public class ThreeAndFiveTest {
+
+    @Test
+    public void that_sum_of_all_multiples_of_three_and_five_are_returned() throws Exception {
+
+        fail();
+    }
+
+}
